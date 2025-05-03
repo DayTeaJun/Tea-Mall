a37ad24bb527795329eaea76ff4765e011532f97
DayTeaJun <gkfnck@gmail.com>
2025-05-03 23:47:14 +0900

    Design: productCard 이미지 엑박 예외 처리

--- app/_components/ProductCard.tsx
+++ app/_components/ProductCard.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { ImageOff } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 interface ProductType {
   id: string;
@@ -17,17 +18,28 @@ interface Props {
 }
 
 function ProductCard({ products }: Props) {
+  const [imageError, setImageError] = useState(false);
+
   return (
     <a
       key={products.id}
       href={`/products/${products.id}`}
-      className="border p-4 rounded-lg shadow hover:shadow-md transition"
+      className="p-4 rounded-lg shadow hover:shadow-md transition"
     >
-      <Image
-        src={products.image_url}
-        alt={products.name}
-        className="w-full h-40 object-cover mb-2"
-      />
+      <div className="w-full h-40 mb-2 relative flex items-center justify-center bg-gray-50">
+        {!imageError && products.image_url ? (
+          <img
+            src={products.image_url}
+            alt={products.name}
+            width={200}
+            height={160}
+            className="object-cover w-full h-full rounded-md"
+            onError={() => setImageError(true)}
+          />
+        ) : (
+          <ImageOff className="text-gray-400" size={40} />
+        )}
+      </div>
       <h3 className="text-lg font-medium">{products.name}</h3>
       <p className="text-gray-500 text-sm truncate">{products.description}</p>
       <p className="text-right mt-2 font-bold">
