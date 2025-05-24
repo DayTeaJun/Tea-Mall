774039a0b3fbed7118bbc84c70bfad4efb08dbaa
DayTeaJun <gkfnck@gmail.com>
2025-05-25 07:23:07 +0900

    Refactor: 장바구니 페이지 api 코드 분리

--- app/myCart/page.tsx
+++ app/myCart/page.tsx
@@ -1,84 +1,30 @@
 "use client";
 
-import { createBrowserSupabaseClient } from "@/lib/config/supabase/client";
+import {
+  useProductAllCart,
+  useUpdateQuantityMutation,
+} from "@/lib/queries/products";
 import { useAuthStore } from "@/lib/store/useAuthStore";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
-const supabase = createBrowserSupabaseClient();
-
-type CartItemType = {
-  id: string;
-  quantity: number;
-  product: {
-    id: string;
-    name: string;
-    price: number;
-    image_url: string | null;
-  } | null;
-};
-
 export default function MyCartPage() {
-  const queryClient = useQueryClient();
   const { user } = useAuthStore();
   const router = useRouter();
+  const { mutate, isPending } = useUpdateQuantityMutation(user?.id ?? "");
 
-  const { data: cartItems = [], isLoading } = useQuery<CartItemType[], Error>({
-    queryKey: ["cartItems", user?.id],
-    enabled: !!user,
-    queryFn: async () => {
-      const { data, error } = await supabase
-        .from("cart_items")
-        .select(
-          `
-        id,
-        quantity,
-        product:product_id (
-          id,
-          name,
-          price,
-          image_url
-        )
-      `,
-        )
-        .eq("user_id", user!.id)
-        .order("created_at", { ascending: true });
+  const { data: cartItems, isLoading } = useProductAllCart(user?.id ?? "");
 
-      if (error) throw error;
+  const totalQuantity =
+    cartItems && cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  const totalPrice =
+    cartItems &&
+    cartItems.reduce(
+      (sum, item) => sum + (item.product?.price ?? 0) * item.quantity,
+      0,
+    );
 
-      return data as CartItemType[]; // ✅ 강제 타입 지정
-    },
-  });
-
-  const updateQuantity = useMutation({
-    mutationFn: async ({
-      itemId,
-      quantity,
-    }: {
-      itemId: string;
-      quantity: number;
-    }) => {
-      if (quantity < 1) return;
-      const { error } = await supabase
-        .from("cart_items")
-        .update({ quantity })
-        .eq("id", itemId);
-      if (error) throw error;
-    },
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["cartItems", user?.id],
-      });
-    },
-  });
-
-  const totalQuantity = cartItems.reduce((sum, item) => sum + item.quantity, 0);
-  const totalPrice = cartItems.reduce(
-    (sum, item) => sum + (item.product?.price ?? 0) * item.quantity,
-    0,
-  );
-
-  if (!user || isLoading) return <div className="p-5">로딩 중...</div>;
+  if (!user || isLoading || !cartItems)
+    return <div className="p-5">로딩 중...</div>;
 
   if (!user) return <div className="p-5">로그인 정보가 없습니다.</div>;
 
@@ -86,7 +32,7 @@ export default function MyCartPage() {
     <div className="p-5">
       <h1 className="text-xl font-bold mb-4">장바구니</h1>
 
-      {cartItems.length === 0 ? (
+      {cartItems.length === 0 || !cartItems ? (
         <p className="text-gray-600">장바구니가 비어 있습니다.</p>
       ) : (
         <>
@@ -121,12 +67,12 @@ export default function MyCartPage() {
                     className="w-8 h-8 border rounded bg-white hover:bg-gray-400 transition-all cursor-pointer"
                     onClick={(e) => {
                       e.stopPropagation();
-                      updateQuantity.mutate({
+                      mutate({
                         itemId: item.id,
                         quantity: item.quantity - 1,
                       });
                     }}
-                    disabled={updateQuantity.isPending}
+                    disabled={isPending}
                   >
                     -
                   </button>
@@ -137,12 +83,12 @@ export default function MyCartPage() {
                     className="w-8 h-8 border rounded bg-white hover:bg-gray-400 transition-all cursor-pointer"
                     onClick={(e) => {
                       e.stopPropagation();
-                      updateQuantity.mutate({
+                      mutate({
                         itemId: item.id,
                         quantity: item.quantity + 1,
                       });
                     }}
-                    disabled={updateQuantity.isPending}
+                    disabled={isPending}
                   >
                     +
                   </button>
@@ -154,7 +100,7 @@ export default function MyCartPage() {
           <div className="border-t pt-4 text-right space-y-1">
             <p className="text-sm text-gray-600">총 수량: {totalQuantity}개</p>
             <p className="text-lg font-bold">
-              총 금액: ₩{totalPrice.toLocaleString()}
+              총 금액: ₩{totalPrice?.toLocaleString() || 0}
             </p>
           </div>
         </>
--- lib/queries/products.ts
+++ lib/queries/products.ts
@@ -2,7 +2,19 @@
 
 import { ProductType } from "@/types/product";
 import { createBrowserSupabaseClient } from "../config/supabase/client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { queryClient } from "@/components/providers/ReactQueryProvider";
+
+export type CartItemType = {
+  id: string;
+  quantity: number;
+  product: {
+    id: string;
+    name: string;
+    price: number;
+    image_url: string | null;
+  };
+};
 
 const supabase = createBrowserSupabaseClient();
 
@@ -29,3 +41,65 @@ export function useProductAllToMainQuery() {
     isError,
   };
 }
+
+// 장바구니 상품 전체 조회
+export async function getProductAllCart(
+  userId: string,
+): Promise<CartItemType[]> {
+  const { data, error } = await supabase
+    .from("cart_items")
+    .select("id, quantity, product:product_id ( id, name, price, image_url )") // 장바구니 및 장바구니 상품 정보 조회 조인
+    .eq("user_id", userId)
+    .order("created_at", { ascending: true });
+
+  if (error) throw error;
+  const normalized = (data ?? []).map((item) => ({
+    ...item,
+    product: Array.isArray(item.product) ? item.product[0] : item.product, // product가 배열로 올 경우 첫 번째 요소만 사용 (supabase에서 조인 시 여러개가 조회될 수 있다 판단하여 배열로 반환)
+  }));
+
+  return normalized;
+}
+
+export function useProductAllCart(userId: string) {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["cart_items", userId],
+    queryFn: () => getProductAllCart(userId),
+  });
+
+  return {
+    data,
+    isLoading,
+    isError,
+  };
+}
+
+// 장바구니 수량 변경
+export const updateCartItemQuantity = async ({
+  itemId,
+  quantity,
+}: {
+  itemId: string;
+  quantity: number;
+}) => {
+  if (quantity < 1) return;
+  const { data, error } = await supabase
+    .from("cart_items")
+    .update({ quantity })
+    .eq("id", itemId);
+
+  if (error) throw error;
+  return data ?? [];
+};
+
+export const useUpdateQuantityMutation = (userId: string) => {
+  const { data, isError, mutate, isSuccess, isPending } = useMutation({
+    mutationFn: updateCartItemQuantity,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["cart_items", userId],
+      });
+    },
+  });
+  return { data, isError, mutate, isSuccess, isPending };
+};
