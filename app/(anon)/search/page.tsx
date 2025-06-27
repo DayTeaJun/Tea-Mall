45dfd918b159f91a8e2be6ead235694f13d8af29
DayTeaJun <gkfnck@gmail.com>
2025-06-27 23:40:39 +0900

    Design: 검색 페이지 UI 수정

--- app/(anon)/search/page.tsx
+++ app/(anon)/search/page.tsx
@@ -12,8 +12,16 @@ export default function SearchPage() {
   const { data: products, isLoading } = useSearchProductsQuery(query);
 
   return (
-    <div className="p-8 w-full">
+    <div className="p-8 w-full max-w-7xl mx-auto">
       <h1 className="text-2xl mb-4 text-center">Search Result</h1>
+      <p className="text-center text-gray-500 mb-8">
+        <span className="text-black font-semibold">
+          &quot;
+          {query}
+          &quot;
+        </span>{" "}
+        에 대한 검색 결과입니다.
+      </p>
 
       {isLoading ? (
         <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
