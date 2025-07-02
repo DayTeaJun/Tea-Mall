56921f1b14ea6c1932b67245ea3cf320b20d0946
DayTeaJun <gkfnck@gmail.com>
2025-07-02 23:54:12 +0900

    Feat: 마이페이지 내정보 추가중

--- app/(member)/mypage/layout.tsx
+++ app/(member)/mypage/layout.tsx
@@ -4,7 +4,7 @@ import SidebarNav from "./_components/SidebarNav";
 export default function MyPageLayout({ children }: { children: ReactNode }) {
   return (
     <div className="max-w-7xl mx-auto p-8 flex gap-8">
-      <aside className="w-1/4 border-r pr-4 h-[calc(100vh-225px)] overflow-y-auto">
+      <aside className="w-1/6 border-r pr-4 h-[calc(100vh-225px)] overflow-y-auto">
         <SidebarNav />
       </aside>
       <main className="flex-1">{children}</main>
--- app/(member)/mypage/profile/page.tsx
+++ app/(member)/mypage/profile/page.tsx
@@ -1,8 +1,34 @@
-export default function ProfilePage() {
+import { createServerSupabaseClient } from "@/lib/config/supabase/server/server";
+import { notFound } from "next/navigation";
+
+export default async function ProfilePage() {
+  const supabase = await createServerSupabaseClient();
+  const { data: user } = await supabase.auth.getUser();
+  const userId = user?.user?.id;
+  if (!userId) return notFound();
+
+  const { data: profile, error: profileError } = await supabase
+    .from("user_table")
+    .select("level")
+    .eq("id", userId)
+    .single();
+
   return (
     <div>
       <h2 className="text-xl font-semibold mb-4">내 정보</h2>
-      <p>이곳에서 프로필을 관리할 수 있습니다.</p>
+      <p>이름</p>
+      <span>{profile?.name}</span>
+      <p>전화번호</p>
+      <span></span>
+
+      <div className="flex gap-2 mt-4">
+        <button className="bg-gray-300 px-2 p-1 rounded">
+          비밀번호 재설정
+        </button>
+        <button className="bg-red-500 text-white px-2 p-1 rounded">
+          회원탈퇴
+        </button>
+      </div>
     </div>
   );
 }
