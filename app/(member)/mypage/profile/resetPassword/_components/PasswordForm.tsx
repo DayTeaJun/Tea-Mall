9f5e2a44697e5f0eb2ffabe5ad32388bd9a48031
DayTeaJun <gkfnck@gmail.com>
2025-07-10 20:30:14 +0900

    Feat: 비밀번호 재설정 컴포넌트 추가

--- app/(member)/mypage/profile/resetPassword/_components/PasswordForm.tsx
+++ app/(member)/mypage/profile/resetPassword/_components/PasswordForm.tsx
@@ -3,74 +3,127 @@
 import React, { useState } from "react";
 import { createBrowserSupabaseClient } from "@/lib/config/supabase/client";
 import { toast } from "sonner";
-import { CircleCheck } from "lucide-react";
 import Link from "next/link";
+import { useAuthStore } from "@/lib/store/useAuthStore";
+import { useRouter } from "next/navigation";
 
 function PasswordForm() {
-  const [email, setEmail] = useState("");
-  const [emailValid, setEmailValid] = useState("");
+  const router = useRouter();
+  const { user } = useAuthStore();
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-  const [step, setStep] = useState<"input" | "sent">("input");
-
-  const isFormValid = emailValid === "중복된 이메일입니다.";
 
   const supabase = createBrowserSupabaseClient();
 
+  const disabled =
+    !currentPassword ||
+    !newPassword ||
+    !newPasswordConfirm ||
+    newPassword !== newPasswordConfirm;
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    if (!isFormValid) return;
+    if (!user) return;
+    if (!currentPassword || !newPassword || !newPasswordConfirm) {
+      toast.warning("모든 항목을 입력해주세요.");
+      return;
+    }
 
     setIsLoading(true);
 
-    const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/reset-password`,
+    const { error: signInError } = await supabase.auth.signInWithPassword({
+      email: user.email,
+      password: currentPassword,
+    });
+
+    if (signInError) {
+      setIsLoading(false);
+      return;
+    }
+
+    const { error: updateError } = await supabase.auth.updateUser({
+      password: newPassword,
     });
 
     setIsLoading(false);
-    setStep("sent");
 
-    if (error) {
-      toast.error("비밀번호 재설정 이메일 전송 실패: " + error.message);
+    if (updateError) {
+      if (
+        updateError.message ===
+        "New password should be different from the old password."
+      ) {
+        toast.error("이전 비밀번호와 일치합니다.");
+      } else if (
+        updateError.message === "Password should be at least 6 characters."
+      ) {
+        toast.error("비밀번호는 최소 6자 이상이어야 합니다.");
+      } else {
+        toast.error(updateError.message);
+      }
     } else {
-      toast.success("비밀번호 재설정 링크가 이메일로 전송되었습니다.");
+      toast.success("비밀번호가 성공적으로 변경되었습니다.");
+
+      router.push("/mypage");
     }
   };
 
-  if (step === "sent") {
-    return (
-      <div className="text-center p-4 flex flex-col items-center gap-2 mt-10">
-        <CircleCheck size={60} className="text-green-500 mb-2" />
-        <p className="font-bold text-lg">
-          비밀번호 재설정 이메일이 전송되었습니다.
-        </p>
-        <p className="text-sm text-gray-500 mt-2">
-          이메일을 확인하고 메일에 첨부된 링크를 클릭해주세요.
-        </p>
-      </div>
-    );
-  }
-
   return (
     <>
-      <p className="text-[13px] text-gray-500">
-        아래 비밀번호 재설정 링크를 눌러 비밀번호 초기화를 진행해주세요.
+      <p className="text-[13px] text-gray-500 mb-2">
+        현재 비밀번호를 입력한 후 새 비밀번호로 변경할 수 있습니다.
       </p>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col gap-2 max-w-[500px] w-full"
+        className="flex flex-col gap-4 max-w-[500px] w-full"
       >
+        <input
+          type="password"
+          placeholder="현재 비밀번호"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+          className="p-3 border rounded-md text-sm"
+        />
+
+        <input
+          type="password"
+          placeholder="새 비밀번호"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          className="p-3 border rounded-md text-sm"
+        />
+
+        <input
+          type="password"
+          placeholder="새 비밀번호 확인"
+          value={newPasswordConfirm}
+          onChange={(e) => setNewPasswordConfirm(e.target.value)}
+          className="p-3 border rounded-md text-sm"
+        />
+
+        <span
+          className={`${
+            newPassword === newPasswordConfirm
+              ? "text-green-500"
+              : "text-red-500"
+          } text-[12px] h-[18px]`}
+        >
+          {newPasswordConfirm.length > 0 &&
+            (newPassword === newPasswordConfirm
+              ? "비밀번호가 일치합니다."
+              : "비밀번호가 일치하지 않습니다.")}
+        </span>
         <button
           type="submit"
-          className={`p-3 rounded-md font-bold transition-all duration-200 ease-in-out ${
-            isFormValid
-              ? "bg-green-600 text-white hover:bg-green-700 cursor-pointer"
-              : "bg-gray-300 text-white cursor-default"
+          disabled={disabled}
+          className={`p-3 rounded-md font-bold transition-all duration-200 ease-in-out text-white ${
+            disabled
+              ? "bg-gray-400 !cursor-default"
+              : "bg-green-600 hover:bg-green-700"
           }`}
-          disabled={!isFormValid || isLoading}
         >
-          {isLoading ? "전송 중..." : "비밀번호 재설정"}
+          {isLoading ? "변경 중..." : "비밀번호 변경"}
         </button>
         <Link
           href="/mypage/profile"
--- app/(member)/mypage/profile/resetPassword/page.tsx
+++ app/(member)/mypage/profile/resetPassword/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import FindPasswordForm from "./_components/PasswordForm";
+import PasswordForm from "./_components/PasswordForm";
 
 function ResetPasswordPage() {
   return (
     <section className="flex flex-col gap-4">
       <h2 className="text-xl font-semibold mb-2">비밀번호 재설정</h2>
-      <FindPasswordForm />
+      <PasswordForm />
     </section>
   );
 }
