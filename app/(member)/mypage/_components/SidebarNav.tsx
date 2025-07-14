42bbc31e59631d6a070faf7ecd4135c5379561ad
DayTeaJun <gkfnck@gmail.com>
2025-07-14 23:59:23 +0900

    Design: 프로필 스켈레톤 ui 추가

--- app/(member)/mypage/_components/SidebarNav.tsx
+++ app/(member)/mypage/_components/SidebarNav.tsx
@@ -17,11 +17,13 @@ export default function SidebarNav() {
 
   return (
     <nav className="flex flex-col h-full bg-white text-sm">
-      <ul className="flex flex-col gap-4 h-[70%]">
-        <p className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 border-b-2 border-gray-400 pb-2 w-fit font-bold">
-          <User size={16} />
-          <span className="text-green-600">{`${user?.user_name}`}</span> 님
-        </p>
+      <ul className="flex flex-col gap-4 h-[50vh]">
+        {user ? (
+          <p className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 border-b-2 border-gray-400 pb-2 w-fit font-bold">
+            <User size={16} />
+            <span className="text-green-600">{`${user?.user_name}`}</span> 님
+          </p>
+        ) : null}
 
         {menu.map(({ name, href }) => {
           const isActive = pathname.startsWith(href);
--- app/(member)/mypage/profile/page.tsx
+++ app/(member)/mypage/profile/page.tsx
@@ -18,7 +18,37 @@ export default function ProfilePage() {
   const { data, isLoading } = useMyProfileQuery(user?.id);
 
   if (isLoading) {
-    return <div>로딩 중...</div>;
+    return (
+      <section className="flex flex-col gap-4 animate-pulse">
+        <div className="h-8 bg-gray-200 rounded w-1/3" />
+        <div className="flex flex-col gap-6">
+          <div className="w-[100px] h-[100px] rounded-full bg-gray-300" />
+
+          <div className="flex flex-col gap-2 mb-4">
+            <div className="h-5 bg-gray-200 rounded w-1/4" />
+            <div className="h-4 bg-gray-100 rounded w-2/3" />
+          </div>
+
+          <div className="flex flex-col gap-2 mb-4">
+            <div className="h-5 bg-gray-200 rounded w-1/4" />
+            <div className="h-4 bg-gray-100 rounded w-1/2" />
+          </div>
+
+          <div className="flex flex-col gap-2 mb-4">
+            <div className="h-5 bg-gray-200 rounded w-1/4" />
+            <div className="h-4 bg-gray-100 rounded w-3/4" />
+          </div>
+        </div>
+
+        <div className="flex justify-between mt-10">
+          <div className="w-28 h-8 bg-gray-300 rounded" />
+          <div className="flex gap-2">
+            <div className="w-36 h-8 bg-gray-200 rounded" />
+            <div className="w-24 h-8 bg-gray-300 rounded" />
+          </div>
+        </div>
+      </section>
+    );
   }
 
   const handleDelete = async () => {
