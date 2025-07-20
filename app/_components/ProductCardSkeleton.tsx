050aaf51745f9c75f6fbe2286dc8e91339bb495d
DayTeaJun <gkfnck@gmail.com>
2025-07-20 23:53:08 +0900

    Design: 검색 페이지 SkeletonUI 수정

--- app/_components/ProductCardSkeleton.tsx
+++ app/_components/ProductCardSkeleton.tsx
@@ -1,7 +1,7 @@
 export default function ProductCardSkeleton() {
   return (
     <div className="w-full transition-all duration-300 p-4 pt-0">
-      <div className="w-full h-80 mb-2 relative bg-gray-100 animate-pulse" />
+      <div className="w-full h-50 lg:h-60 mb-2 relative bg-gray-100 animate-pulse" />
 
       <div className="h-5 bg-gray-200 rounded w-3/4 mb-2 animate-pulse" />
 
