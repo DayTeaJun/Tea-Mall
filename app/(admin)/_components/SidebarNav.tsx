ed5fe7bc5a8bff9e82b130e43165aed320d129b5
DayTeaJun <gkfnck@gmail.com>
2025-08-27 23:54:01 +0900

    Design: 관리자 전용 페이지 Navbar 반응형 추가

--- app/(admin)/_components/SidebarNav.tsx
+++ app/(admin)/_components/SidebarNav.tsx
@@ -2,7 +2,8 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { User, UserCog } from "lucide-react";
+import { Menu, User, UserCog } from "lucide-react";
+import { useState } from "react";
 
 const menu = [
   {
@@ -24,42 +25,112 @@ const menu = [
 
 export default function SidebarNav() {
   const pathname = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => setOpen((v) => !v);
 
   return (
     <nav className="flex flex-col h-full bg-white text-sm">
-      <ul className="flex flex-col gap-4 h-[50vh]">
-        <p className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 border-b-2 border-gray-400 pb-2 w-fit font-bold">
-          <UserCog size={16} />
-          관리자 전용
-        </p>
+      <div className="sm:hidden relative pb-2">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2 items-center">
+            <button
+              type="button"
+              onClick={toggle}
+              aria-expanded={open}
+              aria-controls="mypage-nav-list"
+              className="flex items-center justify-between active:scale-[0.99] transition"
+            >
+              <Menu size={18} />
+            </button>
 
-        {menu.map(({ name, href, currentPage }) => {
-          const isActive = pathname.startsWith(currentPage);
+            <p className="text-xs uppercase flex items-center gap-2 w-fit font-bold text-gray-500">
+              <UserCog size={16} />
+              관리자 전용
+            </p>
+          </div>
 
-          return (
-            <li key={href}>
-              <Link
-                href={href}
-                className={`block px-4 py-2 transition-colors duration-150 ${
-                  isActive
-                    ? "bg-gray-800 text-white font-medium"
-                    : "text-gray-700 hover:bg-gray-100"
-                }`}
-              >
-                {name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+          {/* {detailPage.some(({ href }) => pathname.startsWith(href)) && (
+            <button
+              type="button"
+              onClick={() => router.back()}
+              className="text-gray-600"
+            >
+              <ArrowLeft size={18} />
+            </button>
+          )} */}
+        </div>
 
-      <div className="mt-4 pt-4 border-t">
-        <Link
-          href="/mypage/profile"
-          className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 hover:text-red-400"
+        <ul
+          className={`absolute mt-2 w-full bg-white z-50 overflow-hidden transition-[max-height] duration-300 ease-in-out ${
+            open ? "max-h-96 border-b border-t" : "max-h-0"
+          }`}
         >
-          <User size={14} />내 정보 메뉴 이동
-        </Link>
+          {menu.map(({ name, href, currentPage }) => {
+            const isActive = pathname.startsWith(currentPage);
+
+            return (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`block px-4 py-2 transition-colors duration-150 ${
+                    isActive
+                      ? "bg-gray-800 text-white font-medium"
+                      : "text-gray-700 hover:bg-gray-100"
+                  }`}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+
+          <div className="mt-4 pt-4 border-t">
+            <Link
+              href="/mypage/profile"
+              className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 hover:text-red-400"
+            >
+              <User size={14} />내 정보 메뉴 이동
+            </Link>
+          </div>
+        </ul>
+      </div>
+
+      <div className="hidden md:block">
+        <ul className="flex flex-col gap-4 h-[50vh]">
+          <p className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 border-b-2 border-gray-400 pb-2 w-fit font-bold">
+            <UserCog size={16} />
+            관리자 전용
+          </p>
+
+          {menu.map(({ name, href, currentPage }) => {
+            const isActive = pathname.startsWith(currentPage);
+
+            return (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`block px-4 py-2 transition-colors duration-150 ${
+                    isActive
+                      ? "bg-gray-800 text-white font-medium"
+                      : "text-gray-700 hover:bg-gray-100"
+                  }`}
+                >
+                  {name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="mt-4 pt-4 border-t">
+          <Link
+            href="/mypage/profile"
+            className="text-xs text-gray-500 uppercase mb-2 flex items-center gap-2 hover:text-red-400"
+          >
+            <User size={14} />내 정보 메뉴 이동
+          </Link>
+        </div>
       </div>
     </nav>
   );
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -3,10 +3,15 @@ import SidebarNav from "./_components/SidebarNav";
 
 export default function MyPageLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-7xl mx-auto p-8 flex gap-8">
-      <aside className="w-1/6 border-r pr-4 min-h-[calc(100vh-225px)]">
+    <div className="max-w-7xl mx-auto p-4 sm:p-8 flex flex-col md:flex-row gap-2 sm:gap-8">
+      <div className="block md:hidden">
+        <SidebarNav />
+      </div>
+
+      <aside className="w-1/6 border-r pr-4 min-h-[calc(100vh-225px)] hidden md:block">
         <SidebarNav />
       </aside>
+
       <main className="flex-1">{children}</main>
     </div>
   );
