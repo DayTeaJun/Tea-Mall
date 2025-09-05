5940a2e166455436e65c5ee223ca5ae61a02e8e6
DayTeaJun <gkfnck@gmail.com>
2025-09-05 20:45:07 +0900

    Feat: OAuth 로그인 폼 구글 수정

--- app/(auth)/signin/_components/GoogleLogin.tsx
+++ app/(auth)/signin/_components/GoogleLogin.tsx
@@ -4,6 +4,7 @@ import useHydrate from "@/hooks/useHydrate";
 import { createBrowserSupabaseClient } from "@/lib/config/supabase/client";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { SupabaseClient } from "@supabase/supabase-js";
 import React from "react";
 
 function AuthUIForm() {
@@ -15,9 +16,11 @@ function AuthUIForm() {
 
   return (
     <div className="w-full">
-      <div className="max-w-[500px] mx-auto">
+      <div className="max-w-[500px] mx-auto px-5">
         <Auth
-          supabaseClient={supabase}
+          redirectTo={process.env.NEXT_PUBLIC_SITE_URL}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          supabaseClient={supabase as unknown as SupabaseClient<any, "public">}
           appearance={{ theme: ThemeSupa }}
           onlyThirdPartyProviders
           providers={["google", "github"]}
