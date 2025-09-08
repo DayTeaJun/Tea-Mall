86997c64978caebfac381900922bfc5781eaf16e
DayTeaJun <gkfnck@gmail.com>
2025-09-08 23:46:18 +0900

    Fix: middleware 로그아웃 시 로그인 페이지 이동

--- middleware.ts
+++ middleware.ts
@@ -31,7 +31,7 @@ export async function middleware(request: NextRequest) {
 
   if (!isLoggedIn && isProtected) {
     const url = request.nextUrl.clone();
-    url.pathname = "/not-found";
+    url.pathname = "/signin";
     return NextResponse.redirect(url);
   }
 
