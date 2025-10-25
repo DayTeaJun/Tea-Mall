5f5d7234e854d3dd28d89a9a2edd590297657eae
DayTeaJun <gkfnck@gmail.com>
2025-10-25 23:59:14 +0900

    Design: 마이페이지 navbar 반응형 fixed 수정중

--- app/(member)/mypage/layout.tsx
+++ app/(member)/mypage/layout.tsx
@@ -3,16 +3,18 @@ import SidebarNav from "./_components/SidebarNav";
 
 export default function MyPageLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="max-w-7xl mx-auto p-4 sm:p-8 flex flex-col md:flex-row gap-2 sm:gap-8">
-      <div className="block md:hidden">
-        <SidebarNav />
+    <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-2 sm:gap-8 relative p-4 sm:p-8">
+      <div className="md:hidden">
+        <div className="fixed top-[96px]  w-full z-20">
+          <SidebarNav />
+        </div>
       </div>
 
-      <aside className="w-1/6 border-r pr-4 min-h-[calc(100vh-225px)] hidden md:block">
+      <aside className="hidden md:block w-1/6 border-r pr-4 min-h-[calc(100vh-225px)]">
         <SidebarNav />
       </aside>
 
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 mt-4 sm:mt-0">{children}</main>
     </div>
   );
 }
