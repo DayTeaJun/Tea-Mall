39655ca2eaaf60d1bca9627052f3042675c08b9f
DayTeaJun <gkfnck@gmail.com>
2026-03-24 23:34:01 +0900

    Feat: 고객 관리 페이지 추가

---
+++ app/(admin)/manage/user/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-4">
+      <h2 className="text-xl font-bold">고객 관리</h2>
+    </div>
+  );
+}
+
+export default page;
