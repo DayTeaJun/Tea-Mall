207b273a2ab15b8e97c64de0f925cde0a73c4a3a
DayTeaJun <gkfnck@gmail.com>
2026-05-14 18:19:01 +0900

    Feat: 고객센터 페이지 추가 및 문의목록 테이블 추가

--- app/(anon)/inquiry/page.tsx
+++ app/(anon)/inquiry/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import InquiryLists from "./_components/InquiryLists";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <section className="">
+      <h1>고객 센터</h1>
+      <InquiryLists />
+    </section>
+  );
 }
 
 export default page;
