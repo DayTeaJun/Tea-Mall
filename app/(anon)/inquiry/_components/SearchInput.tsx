442901af7d4fa1e8e7d2dbca6b365ec1a647a222
DayTeaJun <gkfnck@gmail.com>
2026-05-29 17:26:29 +0900

    Design: 고객센터 페이지 select UI 수정

--- app/(anon)/inquiry/_components/SearchInput.tsx
+++ app/(anon)/inquiry/_components/SearchInput.tsx
@@ -18,7 +18,7 @@ function SearchInput() {
   return (
     <div className="flex justify-between items-center gap-2 relative w-full">
       <div className="flex items-center gap-2">
-        <select className="border border-gray-300 rounded-none py-1">
+        <select className="border px-3 py-2 rounded">
           <option value="content">전체</option>
           <option value="title">제목</option>
           <option value="author">작성자</option>
@@ -31,7 +31,7 @@ function SearchInput() {
             onChange={(e) => setSearchInput(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleSearch()}
             placeholder="제목을 검색하세요."
-            className="w-full px-3 py-2 pr-10 border rounded-md text-sm"
+            className="w-full px-3 py-2 pr-10 border text-sm"
           />
 
           <button
