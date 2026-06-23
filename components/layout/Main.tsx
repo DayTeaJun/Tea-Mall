c92a4dc89e413994de892b0e6b6770d0895c74f0
DayTeaJun <gkfnck@gmail.com>
2026-06-23 23:51:50 +0900

    Design: 전체 레이아웃 수정

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,5 +1,13 @@
 function Main({ children }: React.PropsWithChildren) {
-  return <main className="w-full flex-1 mt-24 sm:mt-24">{children}</main>;
+  return (
+    <main className="w-full min-h-screen flex flex-col justify-center items-center mt-24">
+      <div className="flex flex-col items-center justify-center gap-4 w-full">
+        <section className="w-full h-full flex flex-col items-center">
+          {children}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
