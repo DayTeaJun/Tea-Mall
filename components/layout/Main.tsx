00c5b6ce0b7e34ab968fee4314fa05b267058b86
DayTeaJun <gkfnck@gmail.com>
2026-06-24 00:22:49 +0900

    Design: 전체 레이아웃 중앙정렬 수정

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,11 +1,7 @@
 function Main({ children }: React.PropsWithChildren) {
   return (
-    <main className="w-full min-h-screen flex flex-col justify-center items-center mt-24">
-      <div className="flex flex-col items-center justify-center gap-4 w-full">
-        <section className="w-full h-full flex flex-col items-center">
-          {children}
-        </section>
-      </div>
+    <main className="w-full flex-1 flex flex-col justify-center pt-24">
+      {children}
     </main>
   );
 }
