bf9e6bfbaf1c13ca044bf67dd0f9345e86c00de8
DayTeaJun <gkfnck@gmail.com>
2026-07-16 18:52:52 +0900

    Feat: 채팅상담 컴포넌트 분할 및 사용자채팅 UI 추가

--- components/ui/ToolComponents/Chatting.tsx
+++ components/ui/ToolComponents/Chatting.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useAuthStore } from "@/lib/store/useAuthStore";
-import { ChevronRight, UserIcon, X } from "lucide-react";
+import { X } from "lucide-react";
 import React from "react";
+import AdminChattingRoom from "./AdminChattingRoom";
+import UserChatting from "./UserChatting";
 
 interface Props {
   onClose: () => void;
@@ -25,47 +27,8 @@ function Chatting({ onClose }: Props) {
         </button>
       </div>
 
-      <div className="flex-1 overflow-y-auto p-3">
-        {isAdmin ? (
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center justify-between p-3 hover:bg-gray-50 cursor-pointer transition-colors border-b border-gray-100">
-              <div className="w-full flex items-center gap-2">
-                <div className="relative w-12 h-12 shrink-0 rounded-full overflow-hidden bg-gray-100">
-                  {!user.profile_image_url ? (
-                    // <Image
-                    //   fill
-                    //   src={user.profile_image_url}
-                    //   alt={user.user_name || "Profile"}
-                    //   className="object-cover"
-                    // />
-                    <></>
-                  ) : (
-                    <div className="w-full h-full p-1 flex items-center justify-center text-gray-300">
-                      <UserIcon size={48} />
-                    </div>
-                  )}
-                </div>
-
-                <div className="w-full flex flex-col gap-2">
-                  <div className="w-full flex justify-between items-center">
-                    <p className="font-bold text-sm text-gray-700">
-                      Gildong Hong
-                    </p>
-                    <div className="flex gap-1 items-center text-xs text-gray-400">
-                      <span>17:31</span>
-                      <ChevronRight size={14} />
-                    </div>
-                  </div>
-                  <p className="text-12 text-gray-400">상담 내역입니다.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex justify-center items-center h-full text-sm text-gray-400">
-            상담 내역이 없습니다.
-          </div>
-        )}
+      <div className="flex-1 overflow-y-auto">
+        {isAdmin ? <AdminChattingRoom /> : <UserChatting />}
       </div>
     </div>
   );
